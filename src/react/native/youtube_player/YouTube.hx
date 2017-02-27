09f0732ee82d80b2d7b35d07f50bc8b7468c0c11
Jarrio <Jarrio@users.noreply.github.com>
2017-02-27 01:50:55 +0000

    Update YouTube.hx

--- src/react/native/youtube_player/YouTube.hx
+++ src/react/native/youtube_player/YouTube.hx
@@ -2,6 +2,3 @@ package react.native.youtube_player;
 
 @:jsRequire('react-native-youtube')
 extern class YouTube extends react.ReactComponent {}
-
-@:jsRequire('react-native-youtube')
-extern class RCTYoutube {}
